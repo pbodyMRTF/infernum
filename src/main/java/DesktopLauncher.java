9ca8526d45169b2062126a0175a17dff8d19fb9d
MRTF <pbodymrtf@users.noreply.github.com>
2026-03-15 13:06:03 +0300

    feat: add window icon

--- src/main/java/DesktopLauncher.java
+++ src/main/java/DesktopLauncher.java
@@ -7,6 +7,8 @@ class DesktopLauncher {
         config.useVsync(true);
 
         config.setTitle("sübhanallah bune");
+        config.setWindowIcon(Assets.Textures.ENEMY);
+
         // Başlangıçta config dosyasını oku
         GameConfig gameConfig = ConfigManager.loadConfig();
         if ("FULLSCREEN".equals(gameConfig.Screen)) {
