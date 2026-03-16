af84e2f443c3f3ccd3a5a1c855e5b4406f50589a
MRTF <pbodymrtf@users.noreply.github.com>
2026-03-16 16:31:16 +0300

    refactor: remove unused windowed mode logic

--- src/main/java/DesktopLauncher.java
+++ src/main/java/DesktopLauncher.java
@@ -9,13 +9,8 @@ class DesktopLauncher {
         config.setTitle("Infernum");
         config.setWindowIcon(Assets.Textures.ENEMY);
 
-        // Başlangıçta config dosyasını oku
-        GameConfig gameConfig = ConfigManager.loadConfig();
-        if ("FULLSCREEN".equals(gameConfig.Screen)) {
-            config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
-        } else {
-            config.setWindowedMode(1024, 768);
-        }
+
+        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
         new Lwjgl3Application(new Jgame(), config);
 
     }
