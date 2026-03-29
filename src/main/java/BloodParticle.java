52d23b466d569c6f4b5bea14e5c306213fb27d76
MRTF <pbodymrtf@users.noreply.github.com>
2026-03-29 14:40:22 +0300

    refactor: make boolean isDead return dead

--- src/main/java/BloodParticle.java
+++ src/main/java/BloodParticle.java
@@ -30,6 +30,6 @@ public class BloodParticle {
     }
 
     public boolean isDead() {
-        return life <= 0;
+        return dead;
     }
 }
--- src/main/java/toz.java
+++ src/main/java/toz.java
@@ -30,6 +30,6 @@ public class toz {
     }
 
     public boolean isDead() {
-        return life <= 0;
+        return dead;
     }
 }
